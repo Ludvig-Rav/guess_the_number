84275e35a85ebcad1a107931384902b712a8267b
Ludvig-Rav <ludvig.skinstad@gmail.com>
2021-10-15 11:45:28 +0200

    cleaned code + error handling + better function

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import random
+from random import randint
 
 count = 0
 number0 = 1
@@ -32,65 +32,43 @@ def number_game(rand_number):
             if counter == no_of_guesses:
                 choice = input("Sorry, you're out of guesses...\nDo you want to try again? y/n ")
                 if choice == "y":
+                    rand_number = randint(0, 10)
+                    print("Let's go!")
                     counter = 0
                 else:
                     break
-            user_guess = int(input(f"What is your {counter} guess? "))
-            if user_guess < rand_number:
-                print("You are gessing to low, make another guess!\n ")
-            elif user_guess > rand_number:
-                print("Your guess is to high, go again!\n ")
-            else:
-                print("\nYou guessed the correct number, congratulations!!! Please try again if you like! \n Do you want to go again? (y/n) ")
-                if input("Make your choice ") == "y":
-                    print("Go again")  
-                    counter = 0
+            try:
+                user_guess = int(input(f"What is your {counter} guess?\n "))
+                if user_guess < rand_number:
+                    print("You are gessing to low, make another guess!\n ")
+                elif user_guess > rand_number:
+                    print("Your guess is to high, go again!\n ")
                 else:
-                    print("Thanks for playing my game, you are welcome to play again at any time! ")
-                    break
+                    print("\nYou guessed the correct number, congratulations!!! Please try again if you like! \n Do you want to go again? (y/n) ")
+                    if input("Make your choice ") == "y":
+                        rand_number = randint(0, 10)
+                        print("Go again")  
+                        counter = 0
+                    else:
+                        break
+            except ValueError:
+                print("Try an integer instead!\n")
     else:
         print("Leaving the game")
     print("Thanks for playing, you'll be redirected to the start menu.")
-        
-    # while True:
-    #     count += 1
-    #     try:
-    #         if int(menu_choice) == 1:
-    #             number_guess = (input(f"Guess the number! You have maximum 5 guesses, this is your {count} time: "))
-    #             try:
-    #                 if int(number_guess) < rand_number:
-    #                     return "You are gessing to low, make another guess!\n "
-    #                 elif int(number_guess) > rand_number:
-    #                     return "Your guess is to high, go again!\n "
-    #                 else:
-    #                     print("\nYou guessed the correct number, congratulations!!! Please try again if you like! \n Do you want to go again? (y/n) ")
-    #                     if input("Make your choice ") == "y".str.lower:
-    #                         count = 0
-    #                     else:
-    #                         print("Thanks for playing my game, you are welcome to play again at any time! ")
-    #                         break     
-    #             except:
-    #                 print("You can only enter a integer!")
-    #         elif menu_choice == "q":
-    #             print("Thanks for playing my game, welcome back!")
-    #             break
-    #         else:
-    #             ("Test to input a integer instead!")
-    #         if count == 5:
-    #             print("Sorry, you're out of guesses!")
-    #     except:
-    #         print("Now something went really wrong here!")
-
 
 
 while True:
-    random_value = random.randint(0,10)
+    random_value = randint(0,10)
     show_menu()
-    menu_choice = int(input("Make your choice, guess or exit: "))
-    if menu_choice == 1:
-        number_game(random_value)
-    elif menu_choice == 2:
-        print("Thanks for playing, welcome back! ")
-        break
-    else:
-        print("Thats a strange symbol, I can't seem to understand your input?")
+    try:
+        menu_choice = int(input("Make your choice, guess or exit: "))
+        if menu_choice == 1:
+            number_game(random_value)
+        elif menu_choice == 2:
+            print("Thanks for playing, welcome back! ")
+            break
+        else:
+            print("Thats a strange symbol, I can't seem to understand your input?")
+    except ValueError:
+        print("An error occurred, please try another input!")
